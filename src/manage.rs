724a821c26ba84bcd856853f4eabec5129e23884
Matthias Jugan <matthias.jugan@tum.de>
2022-09-09 11:11:10 +0200

    stats on the execution

--- src/manage.rs
+++ src/manage.rs
@@ -1,12 +1,13 @@
 use crate::dvcs::DVCS;
 use crate::process::{LocalProcess, ProcessResponse};
-use crate::regression::{RegressionAlgorithm, TestResult, RegressionPoint, AssignedRegressionPoint};
+use crate::regression::{
+    AssignedRegressionPoint, RegressionAlgorithm, RegressionPoint, TestResult,
+};
 use std::collections::HashMap;
 use std::marker::PhantomData;
 use std::sync::mpsc::{self, RecvError, TryRecvError};
 
-struct ProcessPool<T> 
- {
+struct ProcessPool<T> {
     next_id: u32,
     empty_slots: u32,
     idle_processes: Vec<LocalProcess<T>>,
@@ -14,7 +15,25 @@ struct ProcessPool<T>
     _marker: PhantomData<T>,
 }
 
-pub fn start<S: RegressionAlgorithm, T: DVCS>(core: &mut S, repository: &str, threads: u32, script_path: &str, worktree_location: Option<String>) {
+struct Stats {
+    number_jobs: u32,
+}
+
+impl Stats {
+    fn new() -> Self {
+        Stats { number_jobs: 0 }
+    }
+}
+
+pub fn start<S: RegressionAlgorithm, T: DVCS>(
+    core: &mut S,
+    repository: &str,
+    threads: u32,
+    script_path: &str,
+    worktree_location: Option<String>,
+) {
+    let mut stats = Stats::new();
+
     let (send, recv) = mpsc::channel::<ProcessResponse>();
 
     let mut pool = ProcessPool::<T> {
@@ -25,7 +44,7 @@ pub fn start<S: RegressionAlgorithm, T: DVCS>(core: &mut S, repository: &str, th
         _marker: PhantomData,
     };
 
-    //We assume that there is at least on process available in the first
+    //We assume that there is at least one process available in the first
     //iteration.
     while !core.done() {
         let mut wait = false;
@@ -33,6 +52,7 @@ pub fn start<S: RegressionAlgorithm, T: DVCS>(core: &mut S, repository: &str, th
             crate::regression::AlgorithmResponse::Job(commit) => {
                 let process = load_process(&mut pool, repository, worktree_location.clone());
                 process.run(commit, send.clone(), script_path.to_string());
+                stats.number_jobs += 1;
             }
             crate::regression::AlgorithmResponse::WaitForResult => {
                 wait = true;
@@ -49,7 +69,6 @@ pub fn start<S: RegressionAlgorithm, T: DVCS>(core: &mut S, repository: &str, th
         };
 
         if wait || (pool.idle_processes.is_empty() && pool.empty_slots == 0) {
-            // println!("Waiting …");
             match recv_response(&recv, &mut pool) {
                 Ok((commit, result)) => core.add_result(commit, result),
                 Err(err) => {
@@ -57,7 +76,6 @@ pub fn start<S: RegressionAlgorithm, T: DVCS>(core: &mut S, repository: &str, th
                     break;
                 }
             }
-            // println!("Done waiting …");
         }
 
         loop {
@@ -74,7 +92,6 @@ pub fn start<S: RegressionAlgorithm, T: DVCS>(core: &mut S, repository: &str, th
         }
     } //END LOOP
 
-   
     //Wait for active processes to be done and clean up.
     println!("Wait for active processes to finish!");
     while !pool.active_processes.is_empty() {
@@ -85,24 +102,23 @@ pub fn start<S: RegressionAlgorithm, T: DVCS>(core: &mut S, repository: &str, th
     }
 
     let results = core.results();
-    let points: Vec<&AssignedRegressionPoint> = results.iter().filter_map(|reg| {
-        if let RegressionPoint::Point(point) = reg {
-            Some(point)
-        } else {
-            None
-        }
-    }).collect();
+    let points: Vec<&AssignedRegressionPoint> = results
+        .iter()
+        .filter_map(|reg| {
+            if let RegressionPoint::Point(point) = reg {
+                Some(point)
+            } else {
+                None
+            }
+        })
+        .collect();
 
-    println!("----\n");
-    match points.len() {
-        0 => println!("No regression point was found!"),
-        1 => println!("1 regression point was found!"),
-        _ => println!("{} regression points were found!\n", points.len()),
-    }
+    println!("\n---- STATS ----\n");
+    println!("Commits tested: {}", stats.number_jobs);
+    println!("Regression points: {}", points.len());
+    println!("\n----\n");
 
-    println!("\ntarget, regression point");
     for point in points {
-        println!("\n----\n");
         println!("Target: {}", point.target);
         println!("Regression Point: {}", point.regression_point);
         if let Some(message) = T::get_commit_info(repository, &point.regression_point) {
@@ -112,7 +128,11 @@ pub fn start<S: RegressionAlgorithm, T: DVCS>(core: &mut S, repository: &str, th
     }
 }
 
-fn load_process<'a, T: DVCS>(pool: &'a mut ProcessPool<T>, repository: &str, worktree_location: Option<String>) -> &'a LocalProcess<T> {
+fn load_process<'a, T: DVCS>(
+    pool: &'a mut ProcessPool<T>,
+    repository: &str,
+    worktree_location: Option<String>,
+) -> &'a LocalProcess<T> {
     let available_process = if !pool.idle_processes.is_empty() {
         pool.idle_processes.pop().unwrap()
     } else if pool.empty_slots > 0 {
